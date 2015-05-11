f6ffecb2de75bcb334b1c3261f7282103d9513d4
Matthieu Huin <mhu@enovance.com>
2015-05-11 18:20:34 +0200

    Adds unit testing
    Change-Id: Id1a8bbf5f6644b251245678c7e0821016fea99d4

---
+++ run_tests.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Copyright (C) 2015 eNovance SAS <licensing@enovance.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+echo "$(date) - $(hostname)"
+
+echo "FLAKE8 tests"
+echo "~~~~~~~~~~~~"
+find . -iname "*.py" | grep -v .tox | xargs flake8
+FLAKE8_ERRORS=$?
+echo
+
+echo "ManageSF tests"
+echo "~~~~~~~~~~~~~"
+(rm -Rf .tox; tox)
+MANAGESF_ERRORS=$?
+echo
+
+exit $[${FLAKE8_ERRORS} + ${BASH8_ERRORS} + ${MANAGESF_ERRORS}];
