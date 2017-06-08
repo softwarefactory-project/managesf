f1d08320132db96450deddfcb6312db395685ade
mhuin <mhuin@redhat.com>
2017-06-08 11:09:49 +0200

    Create a stub for API v2
    A "fake" API v2 endpoint is accessible at /api/v2/. For now it
    mimics v1; endpoints will be implemented incrementally.
    
    Add facilities for results pagination.
    
    Task: 448
    
    Change-Id: I8bf0695dfcf51674c7bb8b4843acff1e53f8e3e8

---
+++ managesf/api/v2/base.py
@@ -0,0 +1,47 @@
+#
+# Copyright (C) 2017 Red Hat
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+def paginate(func):
+    """Decorator facility to automatically paginate GET outputs"""
+    def _f(**kwargs):
+        try:
+            skipped = int(kwargs.get('skip', 0))
+        except ValueError:
+            raise ValueError('Invalid starting index')
+        try:
+            limit = int(kwargs.get('limit', 25))
+        except ValueError:
+            raise ValueError('Invalid limit')
+        if skipped < 0:
+            raise ValueError('Invalid starting index')
+        if limit < 0:
+            raise ValueError('Invalid limit')
+        results = func(**kwargs)
+        try:
+            results, total = func(**kwargs)
+        except ValueError:
+            results = func(**kwargs)
+            total = len(results)
+        if not total:
+            total = len(results)
+        # results is expected to be ordered in one way or an other
+        if len(results) > limit:
+            results = results[skipped: skipped + limit]
+        return {'total': total,
+                'skipped': skipped,
+                'limit': limit,
+                'results': results}
+    return _f
