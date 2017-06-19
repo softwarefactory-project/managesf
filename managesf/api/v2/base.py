6bdb5a780a9076fb05998fe5d40e6d82dbca115f
mhuin <mhuin@redhat.com>
2017-06-19 13:54:23 +0200

    Add base mechanics for API v2
    - Generic CRUD manager class
    - Generic service class
    - Generic data, JSON-serializable class
    - improve paginate decorator to handle ordering
    
    Change-Id: Ia5b73c6cc3e2a1da34d265a4d23e3862a375ecd6

--- managesf/api/v2/base.py
+++ managesf/api/v2/base.py
@@ -14,15 +14,35 @@
 # under the License.
 
 
+import abc
+import six
+import json
+
+
+# TODO move exceptions somewhere more generic
+from managesf.services import exceptions as exc
+
+
 def paginate(func):
     """Decorator facility to automatically paginate GET outputs"""
-    def _f(**kwargs):
+    def _f(*args, **kwargs):
+        if 'skip' not in kwargs:
+            kwargs['skip'] = 0
+        if 'limit' not in kwargs:
+            # TODO config param?
+            kwargs['limit'] = 25
+        if 'order_by' not in kwargs:
+            # use first option as default
+            kwargs['order_by'] = args[0].ordering_options[0]
+        elif kwargs['order_by'] not in args[0].ordering_options:
+            msg = 'invalid ordering option, valid ones are: %s'
+            raise ValueError(msg % ', '.join(args[0].ordering_options))
         try:
-            skipped = int(kwargs.get('skip', 0))
+            skipped = int(kwargs['skip'])
         except ValueError:
             raise ValueError('Invalid starting index')
         try:
-            limit = int(kwargs.get('limit', 25))
+            limit = int(kwargs['limit'])
         except ValueError:
             raise ValueError('Invalid limit')
         if skipped < 0:
@@ -30,9 +50,9 @@ def paginate(func):
         if limit < 0:
             raise ValueError('Invalid limit')
         try:
-            results, total = func(**kwargs)
+            results, total = func(*args, **kwargs)
         except ValueError:
-            results = func(**kwargs)
+            results = func(*args, **kwargs)
             total = len(results)
         if not total:
             total = len(results)
@@ -44,3 +64,74 @@ def paginate(func):
                 'limit': limit,
                 'results': results}
     return _f
+
+
+@six.add_metaclass(abc.ABCMeta)
+class BaseCRUDManager(object):
+
+    def __init__(self, *args, **kwargs):
+        self.ordering_options = []
+
+    def get(self, **kwargs):
+        """get one or many items depending on filtering args.
+        'Mandatory' args:
+        skip (int)
+        limit (int)
+        order_by (str)"""
+        raise NotImplementedError
+
+    def create(self, **kwargs):
+        """create an item"""
+        raise NotImplementedError
+
+    def update(self, **kwargs):
+        """update an item"""
+        raise NotImplementedError
+
+    def delete(self, **kwargs):
+        """delete an item"""
+        raise NotImplementedError
+
+
+@six.add_metaclass(abc.ABCMeta)
+class BaseService(object):
+    """Base plugin for a service that can be managed by Software Factory (v2).
+    """
+
+    _config_section = "base"
+    service_name = "base service"
+
+    def __init__(self, conf):
+        self._full_conf = conf
+        try:
+            self.configure_service(conf)
+        except AttributeError:
+            raise Exception(repr(conf))
+
+    def configure_service(self, conf):
+        try:
+            self.conf = getattr(conf, self._config_section, None)
+        except KeyError:
+            msg = ("The %s service is not available" % self._config_section)
+            raise exc.ServiceNotAvailableError(msg)
+        if not self.conf:
+            msg = ("The %s service is not available" % self._config_section)
+            raise exc.ServiceNotAvailableError(msg)
+
+
+class V2Data(object):
+    """Generic class for returned objects that must be JSON-serializable"""
+
+    def to_dict(self):
+        raise NotImplementedError
+
+    def __json__(self):
+        # pecan shortcut
+        return self.to_dict()
+
+
+class V2DataJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, V2Data):
+            return obj.to_dict()
+        return json.JSONEncoder.default(self, obj)
