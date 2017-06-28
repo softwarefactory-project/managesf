eadc5c49c74b258dd5ed8cd6cec110c893915d6a
mhuin <mhuin@redhat.com>
2017-06-28 18:02:58 +0200

    [WIP] API v2: Jobs API, zuul service
    Add an endpoint listing jobs. the Zuul service fetches info from
    Zuul's reporting database, so it shows only jobs that have been
    run at least once.
    
    Depends-On: Iecbe6c406867af81b6b306766d582865f2113607
    Change-Id: Iaa964c6846ecf6fe93b60256bdd4cb4086005a25
    Task: 455

--- managesf/api/v2/builds/services/sfzuul.py
+++ managesf/api/v2/builds/services/sfzuul.py
@@ -102,7 +102,7 @@ class ZuulBuildsManager(builds.BuildServiceManager):
     def __init__(self, conf):
         super(builds.BuildServiceManager, self).__init__(conf)
         dburi = self.conf.get('dburi')
-        self.connection = SQLConnection('managesf',
+        self.connection = SQLConnection('managesf-zuul-builds',
                                         {'dburi': dburi})
         self.status_url = self.conf.get('status_url')
         self.builds = ZuulBuildManager(self)
