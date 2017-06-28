967a6d79e9ef00fc08fd965f91a58384f1ddc0ee
mhuin <mhuin@redhat.com>
2017-06-28 11:10:25 +0200

    API v2: Move manager loading to a dedicated module
    This makes it easier for cross-sharing managers across code.
    
    Change-Id: I53e59d000fe95302f7b6d2fba1d8e3a331230fe1

--- managesf/controllers/api/v2/base.py
+++ managesf/controllers/api/v2/base.py
@@ -21,9 +21,6 @@ from pecan import conf
 from pecan import request
 from pecan.rest import RestController
 
-from stevedore import driver
-import mock
-
 from managesf.model.yamlbkd.engine import SFResourceBackendEngine
 from managesf import policy
 
@@ -41,26 +38,6 @@ def get_user_groups(user_id):
                                      'master')
     groups = resources['resources']['groups']
     return [g for g in groups if user_email in groups[g]['members']]
-
-
-def load_manager(namespace, service):
-    logger.info('loading %s:%s manager' % (namespace, service))
-    # hard-coded Dummy service for testing. What could go wrong?
-    if service == 'DummyService':
-        return mock.MagicMock()
-    try:
-        manager = driver.DriverManager(namespace=namespace,
-                                       name=service,
-                                       invoke_on_load=True,
-                                       invoke_args=(conf,)).driver
-        logger.info('%s:%s manager loaded successfully' % (namespace,
-                                                           service))
-        return manager
-    except Exception as e:
-        msg = 'Could not load manager %s:%s: %s' % (namespace,
-                                                    service, e)
-        logger.error(msg)
-        return None
 
 
 def authorize(rule_name, target):
