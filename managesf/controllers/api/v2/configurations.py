eecb64c2b647df87995b310c5a20134486ae88f1
Tristan Cacqueray <tdecacqu@redhat.com>
2018-05-16 07:48:48 +0000

    wip - configurations: add initial controller and cli
    This change adds an new "configurations" controller to generate
    service configurations based on resources data.
    
    Change-Id: I7e95173c8480bd7c1f878f6b270cf1678ace594c
    Co-Authored-By: Fabien Boucher <fboucher@redhat.com>

---
+++ managesf/controllers/api/v2/configurations.py
@@ -0,0 +1,412 @@
+# Copyright (C) 2018 Red Hat
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import os
+import sys
+import git
+import yaml
+import requests
+
+from pecan import expose
+
+from managesf.controllers.api.v2 import base
+
+
+import unittest
+
+
+class ZuulConfigurationController(base.APIv2RestController):
+    @expose()
+    def get(self, **kwargs):
+        return ZuulTenantsLoad().start()
+
+
+class ConfigurationController:
+    zuul = ZuulConfigurationController()
+
+
+class ZuulTenantsLoad:
+    def __init__(self, url=None):
+        if url is None:
+            url = "http://localhost:20001/v2"
+        self.url = url
+
+    def get_resources(self, url, verify_ssl=True):
+        url = os.path.join(url, "resources")
+        ret = requests.get(url, verify=bool(int(verify_ssl)))
+        return ret.json()
+
+    def fetch_git_repo(self, tenant, url, store, ssl_verify=True):
+        tenant_config_project_path = os.path.join(
+            store, tenant, os.path.basename(url))
+        if not os.path.isdir(tenant_config_project_path):
+            os.makedirs(tenant_config_project_path)
+        if not os.path.isdir(os.path.join(tenant_config_project_path, '.git')):
+            # Clone this is the first time
+            git.Repo.clone_from(
+                url, tenant_config_project_path, branch='master',
+                depth=1, config='http.sslVerify=%s' % bool(int(ssl_verify)))
+        else:
+            # Refresh the repository
+            repo = git.Repo(tenant_config_project_path)
+            repo.remotes.origin.pull()
+        return tenant_config_project_path
+
+    def discover_yaml_files(self, tenants_dir):
+        # Discover all files in config_path
+        paths = []
+        for root, dirs, files in os.walk(tenants_dir, topdown=True):
+            paths.extend([os.path.join(root, path) for path in files])
+            # Keeps only .yaml files
+        paths = filter(
+            lambda x: x.endswith('.yaml') or x.endswith('.yml'), paths)
+        return paths
+
+    def merge_tenant_from_files(
+            self, tenants, tenants_conf_files, tenant_name, projects_list):
+        for path in tenants_conf_files:
+            data = yaml.safe_load(open(path))
+            if not data:
+                continue
+            for tenant in data:
+                self.merge_tenant_from_data(
+                    tenants, tenant, path, tenant_name, projects_list)
+
+    def sanitize_projects_list(self, projects_list):
+        for tenant, tenant_projects_list in projects_list.items():
+            sanitized_project_names = []
+            for kp in tenant_projects_list:
+                if isinstance(kp, dict) and 'projects' in kp.keys():
+                    sanitized_project_names.extend(kp['projects'])
+                elif isinstance(kp, dict):
+                    sanitized_project_names.append(kp.keys[0])
+                else:
+                    sanitized_project_names.append(kp)
+            projects_list[tenant] = sanitized_project_names
+
+    def merge_source(
+            self, tenant_conf, sources, path, tenant_name, projects_list):
+        tenant_sources = tenant_conf.setdefault("source", {})
+        for source in sources.keys():
+            source_conf = tenant_sources.setdefault(source, {})
+            for project_type in sources[source]:
+                projects = source_conf.setdefault(project_type, [])
+                for project in sources[source][project_type]:
+                    if project in projects:
+                        raise RuntimeError(
+                            "%s: define existing project %s for tenant %s"
+                            % (path, project, tenant_name))
+                    projects_list.setdefault(tenant_name, []).append(project)
+                    projects.append(project)
+
+    def merge_tenant_from_data(
+            self, tenants, tenant, path, tenant_name, projects_list):
+        # Merge document
+        if not isinstance(tenant, dict) or not tenant.get('tenant'):
+            raise RuntimeError("%s: invalid tenant block: %s" % (
+                path, tenant
+            ))
+        tenant = tenant.get('tenant')
+        if tenant['name'] != tenant_name:
+            return
+        tenant_conf = tenants.setdefault(
+            tenant['name'], {})
+        for name, value in tenant.items():
+            if name == "source":
+                # Merge source lists
+                self.merge_source(
+                    tenant_conf, value, path, tenant_name, projects_list)
+            elif name != "name":
+                # Set tenant option
+                if name in tenant_conf:
+                    raise RuntimeError(
+                        "%s: define multiple %s for tenant %s" % (
+                            path, tenant["name"], name))
+                tenant_conf[name] = value
+        self.sanitize_projects_list(projects_list)
+
+    def merge_tenant_from_resources(
+            self, tenants, tenant_resources, tenant_name, projects_list):
+        for project in tenant_resources['resources']['projects']:
+            if project['tenant'] != tenant_name:
+                continue
+            for sr in project['source-repositories']:
+                # TODO(fbo): support tenants/zuul-tenant-options
+                sr_name = sr.keys()[0]
+                if sr_name in projects_list[tenant_name]:
+                    # already defined in flat zuul tenant file
+                    continue
+                source = (sr[sr_name].get('connection') or
+                          project['connection'])
+                _project = {}
+                type = 'untrusted-projects'
+                if sr[sr_name].get('zuul/config-project') is True:
+                    type = 'config-projects'
+                    del sr[sr_name]['zuul/config-project']
+                for option, value in sr[sr_name].items():
+                    if option.startswith('zuul/'):
+                        _project[option.replace('zuul/', '')] = value
+                tenants[tenant_name]['source'][source].setdefault(
+                    type, []).append(_project)
+
+    def final_tenant_merge(self, tenants):
+        final_data = []
+        for tenant, tenant_conf in tenants.items():
+            data = {'tenant': {'name': tenant}}
+            data['tenant'].update(tenant_conf)
+            final_data.append(data)
+        return final_data
+
+    def start(self):
+        resources = self.get_resources(self.url)
+        tenants = {}
+        projects_list = {}
+        for tenant_name, data in resources.get(
+                "resources", {}).get("tenants", {}).items():
+            tenant_resources = self.get_resources(data["url"])
+            path = self.fetch_git_repo(
+                tenant_name, tenant_resources["config-repo"],
+                "/var/lib/managesf/git")
+            tenants_dir = os.path.join(path, 'zuul')
+            if not os.path.isdir(tenants_dir):
+                continue
+            tenants_conf_files = self.discover_yaml_files(tenants_dir)
+            self.merge_tenant_from_files(
+                tenants, tenants_conf_files, tenant_name, projects_list)
+            self.merge_tenant_from_resources(
+                tenants, tenant_resources, tenant_name, projects_list)
+
+        final_yaml = self.final_tenant_merge(tenants)
+        return yaml.dump(final_yaml)
+
+
+class TenantsLoadTests(unittest.TestCase):
+
+    def test_merge_tenant_case_1(self):
+        ztl = ZuulTenantsLoad()
+        tenants_data = """
+        - tenant:
+            name: local
+            source:
+              gerrit:
+                config-projects:
+                  - common-config
+        - tenant:
+            name: local
+            max-nodes-per-job: 5
+            source:
+              gerrit:
+                untrusted-projects:
+                  - repo1
+                  - repo2
+        """
+        final_tenants = {}
+        projects_list = {}
+        tenants = yaml.load(tenants_data)
+        for tenant in tenants:
+            ztl.merge_tenant_from_data(
+                final_tenants, tenant, '/data', 'local', projects_list)
+        projects_list_expected = {'local': ['common-config', 'repo1', 'repo2']}
+        self.assertItemsEqual(
+            projects_list['local'], projects_list_expected['local'])
+        final_tenants = ztl.final_tenant_merge(final_tenants)
+        expected = {
+            'tenant': {
+                'name': 'local',
+                'max-nodes-per-job': 5,
+                'source': {
+                    'gerrit': {
+                        'config-projects': ['common-config'],
+                        'untrusted-projects': ['repo1', 'repo2']
+                        }
+                    }
+                }
+            }
+        self.assertDictEqual(final_tenants[0], expected)
+        self.assertEqual(len(final_tenants), 1)
+
+    def test_merge_tenant_case_2(self):
+        ztl = ZuulTenantsLoad()
+        tenant_data = """
+        - tenant:
+            name: local
+            source:
+              gerrit:
+                config-projects:
+                  - config
+        - tenant:
+            name: local
+            source:
+              gerrit:
+                config-projects:
+                  - config
+        """
+        final_tenants = {}
+        projects_list = {}
+        tenants = yaml.load(tenant_data)
+        ztl.merge_tenant_from_data(
+            final_tenants, tenants[0], '/data', 'local', projects_list)
+        projects_list_expected = {'local': ['config']}
+        self.assertItemsEqual(
+            projects_list['local'], projects_list_expected['local'])
+        with self.assertRaises(RuntimeError) as exc:
+            ztl.merge_tenant_from_data(
+                final_tenants, tenants[1], '/data', 'local',  projects_list)
+            self.assertEqual(
+                str(exc),
+                '/data: define existing project config for tenant local')
+
+    def test_merge_tenant_case_3(self):
+        ztl = ZuulTenantsLoad()
+        tenant_data_1 = """
+        - tenant:
+            name: local
+            source:
+              gerrit:
+                config-projects:
+                  - config
+        """
+        tenant_data_2 = """
+        - tenant:
+            name: ansible-network
+            max-nodes-per-job: 5
+            source:
+              gerrit:
+                config-projects:
+                  - config
+              github:
+                untrusted-projects:
+                  - repo1
+                  - repo2
+                  - include: []
+                    projects:
+                      - repo3
+        """
+        final_tenants = {}
+        projects_list = {}
+        tenant1 = yaml.load(tenant_data_1)[0]
+        tenant2 = yaml.load(tenant_data_2)[0]
+        ztl.merge_tenant_from_data(
+            final_tenants, tenant1, '/t1', 'local', projects_list)
+        ztl.merge_tenant_from_data(
+            final_tenants, tenant2, '/t2', 'ansible-network', projects_list)
+        projects_list_expected = {
+            'local': ['config'],
+            'ansible-network': ['config', 'repo1', 'repo2', 'repo3']}
+        self.assertItemsEqual(projects_list['local'],
+                              projects_list_expected['local'])
+        self.assertItemsEqual(projects_list['ansible-network'],
+                              projects_list_expected['ansible-network'])
+        final_tenants = ztl.final_tenant_merge(final_tenants)
+        expected_tenant_ansible_network = {
+            'tenant': {
+                'max-nodes-per-job': 5,
+                'name': 'ansible-network',
+                'source': {
+                    'github': {
+                        'untrusted-projects': [
+                            'repo1',
+                            'repo2',
+                            {'include': [],
+                             'projects': ['repo3']}
+                            ]
+                        },
+                    'gerrit': {
+                        'config-projects': ['config']
+                        }
+                    }
+                }
+            }
+        expected_tenant_local = {
+            'tenant': {
+                'name': 'local',
+                'source': {
+                    'gerrit': {
+                        'config-projects': ['config']
+                        }
+                    }
+                }
+            }
+        self.assertDictEqual(
+            [t for t in final_tenants if
+             t['tenant']['name'] == 'ansible-network'][0],
+            expected_tenant_ansible_network)
+        self.assertDictEqual(
+            [t for t in final_tenants if
+             t['tenant']['name'] == 'local'][0],
+            expected_tenant_local)
+        self.assertEqual(len(final_tenants), 2)
+
+    def test_merge_tenant_case_4(self):
+        tenants = {
+            'local': {
+                'source': {
+                    'gerrit': {
+                        'config-projects': [
+                            'config'
+                        ]
+                    }
+                }
+            },
+            'ansible-network': {
+                'source': {
+                    'github': {
+                        'untrusted-projects': ['repo1', 'repo2']
+                    },
+                    'gerrit': {
+                        'config-projects': ['config']
+                    }
+                },
+                'max-nodes-per-job': 5}
+        }
+        projects_list = {
+            'local': ['config'],
+            'ansible-network': ['repo1', 'repo2', 'config']
+        }
+        resources = {
+            'resources': {
+                'projects': {
+                    'project1': {
+
+                    }
+                }
+            }
+        }
+
+
+def cli():
+    import argparse
+
+    p = argparse.ArgumentParser()
+    p.add_argument("--output")
+    p.add_argument("service", choices=["zuul"])
+    args = p.parse_args()
+
+    if args.service == "zuul":
+        ztl = ZuulTenantsLoad()
+        conf = ztl.start()
+
+    if args.output:
+        open(args.output, "w").write(conf)
+    else:
+        print(conf)
+
+
+if __name__ == "__main__":
+    try:
+        ztl = ZuulTenantsLoad()
+        print(ztl.start())
+    except Exception:
+        print("Unexpected error running %s" % " ".join(sys.argv))
+        raise
