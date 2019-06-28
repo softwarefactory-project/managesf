04eb42172db3947ba9cc2d402f91d6b3263f6be6
Fabien Boucher <fboucher@redhat.com>
2019-06-28 15:17:30 +0000

    wip SCLize managesf
    Change-Id: I293bda33d2e75d60782add9e0a338df6336b7845

--- docs/generate-resources-docs.py
+++ docs/generate-resources-docs.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
-from mock import patch, MagicMock
+import sys
+
+from unittest.mock import patch, MagicMock
+
+m_mock = MagicMock()
 
 # Patch all useless module imports for generating the doc
-m_mock = MagicMock()
 modules = {'managesf.services.gerrit': m_mock,
            'git': m_mock,
            'yaml': m_mock,
@@ -25,6 +28,7 @@ modules = {'managesf.services.gerrit': m_mock,
 
 m_patcher = patch.dict('sys.modules', modules)
 m_patcher.start()
+
 from managesf.model.yamlbkd.resources.gitrepository import GitRepository
 from managesf.model.yamlbkd.resources.project import Project
 from managesf.model.yamlbkd.resources.gitacls import ACL
@@ -34,61 +38,61 @@ from managesf.model.yamlbkd.engine import MAPPING
 
 
 def render_resource(cls):
-    print
-    print cls.MODEL_TYPE
-    print "^"*len(cls.MODEL_TYPE)
-    print
-    print cls.DESCRIPTION
-    print
-    print "Below are the list of keys available for this resource."
-    print
+    print()
+    print(cls.MODEL_TYPE)
+    print("^"*len(cls.MODEL_TYPE))
+    print()
+    print(cls.DESCRIPTION)
+    print()
+    print("Below are the list of keys available for this resource.")
+    print()
     for key, details in cls.MODEL.items():
         if key == "name":
             key = "name (Overwritten by the resource ID)"
-            print
-            print key
-            print '"'*len(key)
-            print "* **Description:** %s" % details[5]
-            print "* **Authorized value:** RE(%s)" % details[1]
-            print "* **Mandatory key:** %s" % details[2]
+            print()
+            print(key)
+            print('"'*len(key))
+            print("* **Description:** %s" % details[5])
+            print("* **Authorized value:** RE(%s)" % details[1])
+            print("* **Mandatory key:** %s" % details[2])
         else:
-            print
-            print key
-            print '"'*len(key)
-            print "* **Description:** %s" % details[5]
-            print "* **Type:** %s" % str(details[0])
-            print "* **Authorized value:** RE(%s)" % str(details[1])
-            print "* **Mandatory key:** %s" % details[2]
-            print "* **Mutable key:** %s" % details[4]
+            print()
+            print(key)
+            print('"'*len(key))
+            print("* **Description:** %s" % details[5])
+            print("* **Type:** %s" % str(details[0]))
+            print("* **Authorized value:** RE(%s)" % str(details[1]))
+            print("* **Mandatory key:** %s" % details[2])
+            print("* **Mutable key:** %s" % details[4])
             if not details[2]:
                 default = details[3]
                 if isinstance(default, str) and not len(default):
                     default = "\"\""
-                print "* **Default value:** %s" % default
+                print("* **Default value:** %s" % default)
 
 if __name__ == '__main__':
-    print ".. _config-resources-model:"
-    print
-    print "Available resources models"
-    print "=========================="
-    print
-    print "Containers / Resources mapping"
-    print "------------------------------"
+    print(".. _config-resources-model:")
+    print()
+    print("Available resources models")
+    print("==========================")
+    print()
+    print("Containers / Resources mapping")
+    print("------------------------------")
     for container, cls in MAPPING.items():
-        print ("* Model type: **%s** can only be defined"
-               " in container: **%s**") % (
-            cls.MODEL_TYPE, container)
-    print
-    print "Resources"
-    print "---------"
-    print
-    print ("Note that the key **name** for every resource type"
-           " is not mandatory. A resource is referenced via"
-           " its resource's ID that will automatically become the resource"
-           " **name** value. The resource's ID must validate the **name**"
-           " value constraint of the related resource type. If you still"
-           " want to specify the name then please make sure to have"
-           " the resource ID equals to the resource name.")
-    print
+        print("* Model type: **%s** can only be "
+              "defined in container: **%s**" % (
+                  cls.MODEL_TYPE, container))
+    print()
+    print("Resources")
+    print("---------")
+    print()
+    print("Note that the key **name** for every resource type"
+          " is not mandatory. A resource is referenced via"
+          " its resource's ID that will automatically become the resource"
+          " **name** value. The resource's ID must validate the **name**"
+          " value constraint of the related resource type. If you still"
+          " want to specify the name then please make sure to have"
+          " the resource ID equals to the resource name.")
+    print()
     for cls in (Project, ACL, GitRepository, Group):
         render_resource(cls)
