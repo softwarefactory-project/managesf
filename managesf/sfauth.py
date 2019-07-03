466edc76a77a50cb01c0b0af952a9a201975a882
Fabien Boucher <fboucher@redhat.com>
2019-07-03 17:55:47 +0200

    Add sfauth to managesf for the time python-sfmanager is moved to scl
    resources.cmd:remote-validate use sfmanager.sfauth.get_cookie
    but for the moment python-sfmanager is still not python3 compatible
    neither scl. It will require some cleaning and even its purpose
    is questionable. So in the meantime import get_cookie in managesf.
    
    Change-Id: Iaff42f00b2ffb73fb5af7e3cc5d9451d4c5183da

---
+++ managesf/sfauth.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2014 eNovance SAS <licensing@enovance.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import json
+try:
+    from urllib.parse import urlparse
+except:
+    # PY2 compat
+    from urlparse import urlparse
+import requests
+
+
+class IntrospectionNotAvailableError(Exception):
+    pass
+
+
+def get_cookie(auth_server,
+               username=None, password=None,
+               github_access_token=None,
+               api_key=None,
+               use_ssl=True,
+               verify=True):
+    # TODO: remove this parameter once
+    #       I8df68b7f74344371e4b45b4a6d1cc3362b70b61e is merged
+    if use_ssl is False:
+        use_ssl = True
+    if urlparse(auth_server).scheme == '':
+        auth_server = "https://%s" % auth_server
+    cauth_info = get_cauth_info(auth_server, verify)
+    url = "%s/auth/login" % auth_server
+    auth_params = {'back': '/',
+                   'args': {}, }
+    methods = cauth_info['service']['auth_methods']
+    if (username and password and ('Password' in methods)):
+        auth_params['args'] = {'username': username,
+                               'password': password}
+        auth_params['method'] = 'Password'
+    elif (github_access_token and
+          ('GithubPersonalAccessToken' in methods)):
+        auth_params['args'] = {'token': github_access_token}
+        auth_params['method'] = 'GithubPersonalAccessToken'
+    elif (api_key and ('APIKey' in methods)):
+        auth_params['args'] = {'api_key': api_key}
+        auth_params['method'] = 'APIKey'
+    else:
+        m = "Missing credentials (accepted auth methods: %s)"
+        methods = ','.join(methods)
+        raise ValueError(m % methods)
+    header = {'Content-Type': 'application/json'}
+    resp = requests.post(url, json.dumps(auth_params, sort_keys=True),
+                         allow_redirects=False,
+                         verify=verify,
+                         headers=header)
+    return resp.cookies.get('auth_pubtkt', '')
+
+
+def _get_service_info(url, verify=True):
+    resp = requests.get(url, allow_redirects=False,
+                        verify=verify)
+    if resp.status_code > 399:
+        raise IntrospectionNotAvailableError()
+    return resp.json()
+
+
+def get_cauth_info(auth_server, verify=True):
+    url = "%s/auth/about/" % auth_server
+    return _get_service_info(url, verify)
+
+
+def get_managesf_info(auth_server, verify=True):
+    url = "%s/about/" % auth_server
+    return _get_service_info(url, verify)
