a64ea1fc06b12bf66b34bbd95bdd2d420095eb4f
Matthieu Huin <mhuin@redhat.com>
2020-02-05 17:36:42 +0100

    Remove Storyboard-related code
    Change-Id: Id24a130b7ce45b3c5922d087a0cb2b6719ad07fb

--- managesf/tests/test_resources_project.py
+++ managesf/tests/test_resources_project.py
@@ -29,66 +29,18 @@ class ProjectOpsTest(TestCase):
 
     def test_create(self):
         p = ProjectOps(self.conf, None)
-        with patch.object(p.stb_ops, 'is_activated') as is_activated:
-            is_activated.return_value = True
-            with patch.object(p.stb_ops, 'update_project_groups') as c:
-                logs = p.create()
-                self.assertTrue(c.called)
-                self.assertEqual(len(logs), 0)
-            is_activated.return_value = False
-            with patch.object(p.stb_ops, 'update_project_groups') as c:
-                logs = p.create()
-                self.assertFalse(c.called)
-                self.assertEqual(len(logs), 0)
-            is_activated.return_value = True
-            with patch.object(p.stb_ops, 'update_project_groups') as c:
-                c.side_effect = Exception('xyz')
-                logs = p.create()
-                self.assertTrue(c.called)
-                self.assertEqual(len(logs), 1)
-                self.assertIn('xyz', logs[0])
+        logs = p.create()
+        self.assertEqual(len(logs), 0)
 
     def test_update(self):
         p = ProjectOps(self.conf, None)
-        with patch.object(p.stb_ops, 'is_activated') as is_activated:
-            is_activated.return_value = True
-            with patch.object(p.stb_ops, 'update_project_groups') as c:
-                logs = p.update()
-                self.assertTrue(c.called)
-                self.assertEqual(len(logs), 0)
-            is_activated.return_value = False
-            with patch.object(p.stb_ops, 'update_project_groups') as c:
-                logs = p.update()
-                self.assertFalse(c.called)
-                self.assertEqual(len(logs), 0)
-            is_activated.return_value = True
-            with patch.object(p.stb_ops, 'update_project_groups') as c:
-                c.side_effect = Exception('xyz')
-                logs = p.update()
-                self.assertTrue(c.called)
-                self.assertEqual(len(logs), 1)
-                self.assertIn('xyz', logs[0])
+        logs = p.update()
+        self.assertEqual(len(logs), 0)
 
     def test_delete(self):
         p = ProjectOps(self.conf, None)
-        with patch.object(p.stb_ops, 'is_activated') as is_activated:
-            is_activated.return_value = True
-            with patch.object(p.stb_ops, 'delete_project_groups') as c:
-                logs = p.delete()
-                self.assertTrue(c.called)
-                self.assertEqual(len(logs), 0)
-            is_activated.return_value = False
-            with patch.object(p.stb_ops, 'delete_project_groups') as c:
-                logs = p.delete()
-                self.assertFalse(c.called)
-                self.assertEqual(len(logs), 0)
-            is_activated.return_value = True
-            with patch.object(p.stb_ops, 'delete_project_groups') as c:
-                c.side_effect = Exception('xyz')
-                logs = p.delete()
-                self.assertTrue(c.called)
-                self.assertEqual(len(logs), 1)
-                self.assertIn('xyz', logs[0])
+        logs = p.delete()
+        self.assertEqual(len(logs), 0)
 
     def test_extra_validations(self):
         new = {
@@ -102,15 +54,5 @@ class ProjectOpsTest(TestCase):
         }
         p = ProjectOps(self.conf, new)
         kwargs = {'name': 'p1'}
-        with patch.object(p.stb_ops, 'is_activated') as is_activated:
-            is_activated.return_value = True
-            with patch.object(p.stb_ops, 'extra_validations') as c:
-                c.return_value = []
-                logs = p.extra_validations(**kwargs)
-                self.assertTrue(c.called)
-                self.assertEqual(len(logs), 0)
-            is_activated.return_value = False
-            with patch.object(p.stb_ops, 'extra_validations') as c:
-                logs = p.extra_validations(**kwargs)
-                self.assertFalse(c.called)
-                self.assertEqual(len(logs), 0)
+        logs = p.extra_validations(**kwargs)
+        self.assertEqual(len(logs), 0)
